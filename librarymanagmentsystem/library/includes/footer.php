5b59bd7327341edf6b9c8d10cbb34e1ae248eb34
muzamil-lab <muhammadmuzamil280@gmail.com>
2020-12-29 12:50:51 +0500

    12:50
    UPDATES

--- librarymanagmentsystem/library/includes/footer.php
+++ librarymanagmentsystem/library/includes/footer.php
@@ -2,7 +2,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                   &copy; 2017 Online Library Management System |<a href="https://phpgurukul.com/" target="_blank"  > Designed by : PHPGURUKUL</a> 
+                   &copy; 2020 Online Library Management System
                 </div>
 
             </div>
