0acb39eeb36e0fc2fbde6d2711f9793bd0fa8162
usmanmaqsood263 <usmanmaqsood.263@gmail.com>
2020-12-30 14:31:50 +0500

    Updated Dtatabase
    changes in Database

--- librarymanagmentsystem/sql/library.sql
+++ librarymanagmentsystem/sql/library.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 28, 2020 at 08:53 PM
+-- Generation Time: Dec 30, 2020 at 10:28 AM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.2.34
 
@@ -65,7 +65,9 @@ INSERT INTO `tblauthors` (`id`, `AuthorName`, `creationDate`, `UpdationDate`) VA
 (2, 'Somaya Zaman', '2020-12-20 14:30:23', '2020-12-20 14:30:23'),
 (3, 'Dr Usman Khalid', '2020-12-20 14:30:23', '2020-12-20 14:30:23'),
 (4, 'muhammad fahab', '2020-11-20 14:30:23', '2020-12-28 16:36:44'),
-(5, 'Dr Sehrish Gul', '2020-12-10 14:30:23', '2020-12-10 14:30:23');
+(5, 'Dr Sehrish Gul', '2020-12-10 14:30:23', '2020-12-10 14:30:23'),
+(10, 'Mukhtiar Zamin', '2020-12-30 08:30:21', NULL),
+(11, 'Samera Bano', '2020-12-30 08:43:48', NULL);
 
 -- --------------------------------------------------------
 
@@ -89,8 +91,11 @@ CREATE TABLE `tblbooks` (
 --
 
 INSERT INTO `tblbooks` (`id`, `BookName`, `CatId`, `AuthorId`, `ISBNNumber`, `BookPrice`, `RegDate`, `UpdationDate`) VALUES
-(1, 'PHP And MySql programming', 5, 1, 222333, 20, '2017-07-08 20:04:55', '2017-07-15 05:54:41'),
-(3, 'physics', 6, 4, 1111, 15, '2017-07-08 20:17:31', '2017-07-15 06:13:17');
+(1, 'PHP And MySql programming', 5, 1, 3322, 20, '2019-12-30 08:31:58', '2020-12-30 09:24:39'),
+(3, 'physics', 6, 4, 1111, 15, '2020-11-30 08:31:58', '2020-12-30 09:23:24'),
+(4, 'Fundamental Of Programming', 8, 1, 1122, 40, '2020-12-30 08:31:58', NULL),
+(5, 'Fundamental Of Logic', 9, 5, 1123, 40, '2020-12-30 08:33:08', '2020-12-30 08:34:52'),
+(6, 'Calculus', 10, 11, 2233, 20, '2020-12-30 08:45:24', NULL);
 
 -- --------------------------------------------------------
 
@@ -111,9 +116,9 @@ CREATE TABLE `tblcategory` (
 --
 
 INSERT INTO `tblcategory` (`id`, `CategoryName`, `Status`, `CreationDate`, `UpdationDate`) VALUES
-(5, 'Technology', 1, '2017-07-04 18:35:39', '2017-07-08 17:13:03'),
-(6, 'Science', 1, '2017-07-04 18:35:55', '0000-00-00 00:00:00'),
-(7, 'Management', 0, '2017-07-04 18:36:16', '0000-00-00 00:00:00');
+(8, 'Computer Science', 1, '2020-12-30 08:29:10', '0000-00-00 00:00:00'),
+(9, 'Computer Engineering', 1, '2020-12-30 08:29:43', '0000-00-00 00:00:00'),
+(10, 'Maths', 1, '2020-12-30 08:42:34', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -136,12 +141,8 @@ CREATE TABLE `tblissuedbookdetails` (
 --
 
 INSERT INTO `tblissuedbookdetails` (`id`, `BookId`, `StudentID`, `IssuesDate`, `ReturnDate`, `RetrunStatus`, `fine`) VALUES
-(1, 1, 'SID002', '2017-07-15 06:09:47', '2017-07-15 11:15:20', 1, 0),
-(2, 1, 'SID002', '2017-07-15 06:12:27', '2017-07-15 11:15:23', 1, 5),
-(3, 3, 'SID002', '2017-07-15 06:13:40', NULL, 0, NULL),
-(4, 3, 'SID002', '2017-07-15 06:23:23', '2017-07-15 11:22:29', 1, 2),
-(5, 1, 'SID009', '2017-07-15 10:59:26', NULL, 0, NULL),
-(6, 3, 'SID011', '2017-07-15 18:02:55', NULL, 0, NULL);
+(7, 4, 'SID01', '2020-12-30 08:36:40', '2020-12-30 09:25:22', 1, 20),
+(8, 4, 'SID015', '2020-12-30 08:59:11', NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -166,7 +167,13 @@ CREATE TABLE `tblstudents` (
 --
 
 INSERT INTO `tblstudents` (`id`, `StudentId`, `FullName`, `EmailId`, `MobileNumber`, `Password`, `Status`, `RegDate`, `UpdationDate`) VALUES
-(1, 'SID001', 'Sanaullah Khan', 'vipsanaullahkhan.2000@gmail.com', '3118883615', '7af4896825dfc7e94f8a1d6846a5a2d4', 1, '2020-12-28 13:03:59', '2020-12-26 13:03:59');
+(1, 'SID01', 'Sanaullah Khan', 'vipsanaullahkhan.2000@gmail.com', '3118883615', '7af4896825dfc7e94f8a1d6846a5a2d4', 1, '2020-12-28 13:03:59', '2020-12-30 09:19:55'),
+(12, 'SID02', 'Sardar Ali Irtaza', 'sardarali.111@gmail.com', '3114254257', '50d8bd62fbe46030f0b8fe022f80ab26', 1, '2020-12-30 08:49:56', '2020-12-30 09:15:42'),
+(13, 'SID03', 'Muhammad Muzamil', 'muzamil.111@gmail.com', '3105025265', '3b84183b2f82038c16e0b6b444f48817', 1, '2020-12-30 08:52:22', '2020-12-30 09:15:49'),
+(14, 'SID05', 'Sardar Badar ', 'badar.111@gmail.com', '3101525136', 'dffcec7e95a5b7b3df08820b89601337', 1, '2020-12-30 08:53:40', '2020-12-30 09:16:04'),
+(15, 'SID04', 'Samama Saleem', 'samama.263@gmail', '3102564875', '32f69488f68c76a307db13525a47db5a', 1, '2020-12-30 08:55:08', '2020-12-30 09:15:57'),
+(16, 'SID017', 'Laraib', 'laraib.123@gmail.com', '3105023255', '8257d23d35ebc9018f744c3069ee5add', 1, '2020-12-30 09:12:39', '2020-12-30 09:15:14'),
+(17, 'SID018', 'Hamid Nawaz', 'hamid.123@gmail.com', '3105023265', '0d090b8dc3919dfdf02ce618193a015a', 1, '2020-12-30 09:19:08', NULL);
 
 --
 -- Indexes for dumped tables
@@ -223,31 +230,31 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `tblauthors`
 --
 ALTER TABLE `tblauthors`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `tblbooks`
 --
 ALTER TABLE `tblbooks`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `tblcategory`
 --
 ALTER TABLE `tblcategory`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `tblissuedbookdetails`
 --
 ALTER TABLE `tblissuedbookdetails`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `tblstudents`
 --
 ALTER TABLE `tblstudents`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
